b5f905a1b95dff597b16589db409116c50a7a8b7
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-07-24 16:42:31 +0530

    filling the content

--- src/components/ui/buttonnew.tsx
+++ src/components/ui/buttonnew.tsx
@@ -55,7 +55,7 @@ export const Blackbutton = ({
         className="group relative inline-flex h-[calc(48px+8px)] items-center justify-center rounded-full bg-neutral-950 py-1 pl-6 pr-14 font-thin text-neutral-50"
       >
         <span className="z-10 pr-2">{children}</span>
-        <div className="absolute right-1 inline-flex h-12 w-12 items-center justify-end rounded-full bg-indigo-500 transition-[width] group-hover:w-[calc(100%-8px)]">
+        <div className="absolute right-1 inline-flex h-12 w-12 items-center justify-end rounded-full bg-indigo-800 transition-[width] group-hover:w-[calc(100%-8px)]">
           <div className="mr-3.5 flex items-center justify-center">
             <svg
               width="15"
