76c6f4d5957f486e21114dd5b8841d585bf80173
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-08-27 22:57:36 +0530

    AWS

--- src/components/ui/threeDmac.tsx
+++ src/components/ui/threeDmac.tsx
@@ -1,41 +1,38 @@
 "use client";
 import { ThreeDMarquee } from "./3d-marquee";
 
-// Import local images from AS3 folder
-import s1 from "../../assets/AS3/s1.png";
-import s2 from "../../assets/AS3/s2.png";
-import s3 from "../../assets/AS3/s3.png";
-import s4 from "../../assets/AS3/s4.png";
-import s5 from "../../assets/AS3/s5.png";
-
 export default function ThreeDMarqueeDemo() {
+  // Use S3 hosted images for faster loading
+  const s3BaseUrl = "https://5kmrr-images-2025.s3.amazonaws.com/screenshots";
+
   const images = [
-    s1,
-    s2,
-    s3,
-    s4,
-    s5,
-    s1, // Repeat some images to ensure smooth loop
-    s2,
-    s3,
-    s4,
-    s5,
-    s1,
-    s2,
-    s3,
-    s4,
-    s5,
-    s1,
-    s2,
-    s3,
-    s4,
-    s5,
-    s1,
-    s2,
-    s3,
-    s4,
-    s5,
+    `${s3BaseUrl}/s1.png`,
+    `${s3BaseUrl}/s2.png`,
+    `${s3BaseUrl}/s3.png`,
+    `${s3BaseUrl}/s4.png`,
+    `${s3BaseUrl}/s5.png`,
+    `${s3BaseUrl}/s1.png`, // Repeat some images to ensure smooth loop
+    `${s3BaseUrl}/s2.png`,
+    `${s3BaseUrl}/s3.png`,
+    `${s3BaseUrl}/s4.png`,
+    `${s3BaseUrl}/s5.png`,
+    `${s3BaseUrl}/s1.png`,
+    `${s3BaseUrl}/s2.png`,
+    `${s3BaseUrl}/s3.png`,
+    `${s3BaseUrl}/s4.png`,
+    `${s3BaseUrl}/s5.png`,
+    `${s3BaseUrl}/s1.png`,
+    `${s3BaseUrl}/s2.png`,
+    `${s3BaseUrl}/s3.png`,
+    `${s3BaseUrl}/s4.png`,
+    `${s3BaseUrl}/s5.png`,
+    `${s3BaseUrl}/s1.png`,
+    `${s3BaseUrl}/s2.png`,
+    `${s3BaseUrl}/s3.png`,
+    `${s3BaseUrl}/s4.png`,
+    `${s3BaseUrl}/s5.png`,
   ];
+
   return (
     <div className="mx-auto my-10 max-w-7xl rounded-3xl bg-gray-950/5 p-2 ring-1 ring-neutral-700/10 dark:bg-neutral-800">
       <ThreeDMarquee images={images} />
