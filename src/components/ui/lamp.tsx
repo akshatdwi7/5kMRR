9a73e385f1ae25e3da90cea437a895edc0996cf3
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-08-30 12:09:54 +0530

    aded

--- src/components/ui/lamp.tsx
+++ src/components/ui/lamp.tsx
@@ -33,9 +33,9 @@ export default function LampDemo() {
           duration: 0.8,
           ease: "easeInOut",
         }}
-        className="mt-8 bg-gradient-to-br text-slate-300 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+        className="mt-8 bg-gradient-to-br from-slate-200 to-slate-400 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
       >
-        Connect your <br /> favorite brokers
+        Connect your <br /> favorite brokerssssss
       </motion.h1>
 
       {/* Company Logos - Smooth Moving Marquee */}
