68525b2a1073e7c195972966b48b17f37218893a
Akshat Dwivedi <akshatdwi755@gmail.com>
2025-09-02 22:02:54 +0530

    added pir chart

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 import * as RechartsPrimitive from "recharts";
 
-import { cn } from "../../lib/util.ts";
+import { cn } from "../../lib/utils";
 
 // Format: { THEME_NAME: CSS_SELECTOR }
-const THEMES = { dark: "" } as const;
+const THEMES = { light: "", dark: ".dark" } as const;
 
 export type ChartConfig = {
   [k in string]: {
